652f570b3fedcb5837b611a734bbaf13fb78034a
佳欣 <bear920211@gmail.com>
2025-09-01 15:17:18 +0800

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-<script>
-  function showIntro() {
-    const intro = document.getElementById('jobIntro');
-    intro.style.display = 'block';
+function showIntro() {
+  const intro = document.getElementById('jobIntro');
+  const isHidden = intro.style.display === 'none' || intro.style.display === '';
+  intro.style.display = isHidden ? 'block' : 'none';
+  if (isHidden) {
+    intro.scrollIntoView({ behavior: 'smooth' });
   }
-</script>
+}
